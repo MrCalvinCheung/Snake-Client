f9c7a5b7d7c7111c35c25728e94084c15903e05e
MrCalvinCheung <calvin_c_hk@hotmail.com>
2015-11-12 16:41:17 +0100

    Oprettet Screen klasse klassen er overført fra Eclipse

--- src/UI/Screen.java
+++ src/UI/Screen.java
@@ -1,6 +1,7 @@
 package UI;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 /**
@@ -16,6 +17,49 @@ public class Screen {
     private Login login;
     private UserMenu menuUser;
 
+    /**
+     * Create the frame.
+     */
+    public Screen() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 497, 317);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+        contentPane.setLayout(new CardLayout(0,0));
+
+        login = new Login();
+        contentPane.add(login, LOGIN);
+
+        menuUser = new UserMenu();
+        contentPane.add(menuUser, MENU_USER);
+
+        c = (CardLayout) getContentPane().getLayout();
+
+
+
+		/*c = new CardLayout();
+		c.addLayoutComponent(login, LOGIN);
+		c.addLayoutComponent(menuUser, MENU_USER);*/
+
+    }
+
+    public Login getlogin() {
+        return login;
+    }
+
+    public UserMenu getmenuUser() {
+        return menuUser;
+    }
+
+    public void show(String card) {
+        c.show(this.getContentPane(), card);
+    }
 
 
 }
+
+
+
+
